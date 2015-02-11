e3aca01050c2fe0a9963febffb1b84244de10d09
Peter Ching <peter.ching@gmail.com>
2015-02-11 00:12:44 -0500

    Create associations in the photo and gallery models.

--- db/schema.rb
+++ db/schema.rb
@@ -11,16 +11,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150210211940) do
+ActiveRecord::Schema.define(version: 20150211042706) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "galleries", force: true do |t|
+    t.string   "title"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "photos", force: true do |t|
     t.string   "title"
     t.string   "image"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "gallery_id"
   end
+
+  add_index "photos", ["gallery_id"], name: "index_photos_on_gallery_id", using: :btree
 
 end
