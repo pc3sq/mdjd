3b44bfcb89990f91201b25e788311c6c37e61907
Peter Ching <peter.ching@gmail.com>
2015-02-11 19:59:53 -0500

    Fix thumbnail versioning.

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -31,7 +31,7 @@ class ImageUploader < CarrierWave::Uploader::Base
   #   # do something
   # end
 
-  Create different versions of your uploaded files:
+  #Create different versions of your uploaded files:
   version :thumb do
     process :resize_to_limit => [200, 200]
   end
